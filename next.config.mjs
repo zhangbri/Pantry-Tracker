57f3907e66292692c0d8389b3d478471e90f5a2e
Brian <zhangbri@umich.edu>
2024-07-31 18:06:36 -0400

    Resolved merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
     experimental: {
       appDir: true,
@@ -7,9 +5,4 @@ const nextConfig = {
     reactStrictMode: true,
   };
   
-  module.exports = nextConfig;
-=======
-const nextConfig = {};
-
-export default nextConfig;
->>>>>>> bfc5bde6f048957689041516d0b1e2c7e76ae943
+  export default nextConfig;
